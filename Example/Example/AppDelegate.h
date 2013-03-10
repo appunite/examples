fe91b7e1941c927b93daf7381e549dc53b680e4e
emil.wojtaszek <emil.wojtaszek@gmail.com>
2013-03-10 17:38:42 +0100

    add project

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Emil Wojtaszek on 10.03.2013.
+//  Copyright (c) 2013 AppUnite.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Example/Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Emil Wojtaszek on 10.03.2013.
+//  Copyright (c) 2013 AppUnite.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
